d604def3d5b21f1416888a12a3d95b9f48c26b4d
gabusleon <gabrielleonp@hotmail.com>
2013-04-02 22:55:19 +0200

    cambio listarEmpresa

--- src/pfm/beans/empresa/ListarEmpresa.java
+++ src/pfm/beans/empresa/ListarEmpresa.java
@@ -17,6 +17,7 @@ public class ListarEmpresa implements Serializable {
 	@ManagedProperty(value = "#{DAOFactory.empresaDAO}")
 	private EmpresaDAO empresaDAO;
 	private List<Empresa> lista = new ArrayList<Empresa>();
+	private List<Empresa> filtered = new ArrayList<Empresa>();
 
 	public ListarEmpresa() {
 	}
@@ -35,12 +36,26 @@ public class ListarEmpresa implements Serializable {
 		String order = "razonSocial";
 		int index = -1;
 		int size = -1;
-		setLista(empresaDAO.find(attributes, values, order, index, size));
+		setLista(empresaDAO.find(attributes, values, order, index, size));		
 		return lista;
 	}
 
 	public void setLista(List<Empresa> lista) {
 		this.lista = lista;
-	}	
+	}
+
+	public List<Empresa> getFiltered() {
+		String[] attributes = {};
+		String[] values = {};
+		String order = "razonSocial";
+		int index = -1;
+		int size = -1;
+		setFiltered(empresaDAO.find(attributes, values, order, index, size));
+		return filtered;
+	}
+
+	public void setFiltered(List<Empresa> filtered) {
+		this.filtered = filtered;
+	}
 
 }
