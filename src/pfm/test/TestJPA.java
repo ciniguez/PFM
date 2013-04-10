e382baefd9916355377f9ff6c8399ee3b718e69b
macintosh <carlos.iniguez79@gmail.com>
2013-04-10 21:49:02 +0200

    Reemplazado por problemas en PULL

--- src/pfm/test/TestJPA.java
+++ src/pfm/test/TestJPA.java
@@ -28,7 +28,6 @@ public class TestJPA {
 			pr.setCategoria(JPADAOFactory.getFactory().getCategoriaDAO().read(2));
 			System.out.println(pr.toString());
 			JPADAOFactory.getFactory().getProductoDAO().update(pr);
-			System.out.println(pr.toString());
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
