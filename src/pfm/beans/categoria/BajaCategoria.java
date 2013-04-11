37dbf669613bce89fceda8c6544eca2d55e22522
macintosh <carlos.iniguez79@gmail.com>
2013-04-11 10:47:42 +0200

    Cambios en paquete Categoria, para cumplir con el diseño estipulado el 10/04/2013

---
+++ src/pfm/beans/categoria/BajaCategoria.java
@@ -0,0 +1,52 @@
+package pfm.beans.categoria;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
+import pfm.dao.CategoriaDAO;
+import pfm.entidades.Categoria;
+
+@ManagedBean(name = "bajaCategoria")
+public class BajaCategoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@ManagedProperty(value = "#{DAOFactory.categoriaDAO}")
+	private CategoriaDAO categoriaDAO;
+	private Categoria categoria;
+
+	public BajaCategoria() {
+	}
+
+	public CategoriaDAO getCategoriaDAO() {
+		return categoriaDAO;
+	}
+
+	public void setCategoriaDAO(CategoriaDAO categoriaDAO) {
+		this.categoriaDAO = categoriaDAO;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String baja() {
+		try {
+			categoria.setEliminado(true);
+			getCategoriaDAO().update(categoria);
+			FacesMessage msg = new FacesMessage("Categoria dada de baja", String.valueOf(categoria.getId()));
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} catch (Exception e) {
+			FacesMessage msg = new FacesMessage("Error", "Categoria no dada de baja");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+		return "listarCategoria";
+	}
+}
---
+++ src/pfm/beans/categoria/CrearCategoria.java
@@ -0,0 +1,54 @@
+package pfm.beans.categoria;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
+import pfm.dao.CategoriaDAO;
+import pfm.entidades.Categoria;
+
+@ManagedBean(name = "crearCategoria")
+public class CrearCategoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@ManagedProperty(value = "#{DAOFactory.categoriaDAO}")
+	private CategoriaDAO categoriaDAO;
+	private Categoria categoria = new Categoria();
+
+	public CrearCategoria() {
+
+	}
+
+	public CategoriaDAO getCategoriaDAO() {
+		return categoriaDAO;
+	}
+
+	public void setCategoriaDAO(CategoriaDAO categoriaDAO) {
+		this.categoriaDAO = categoriaDAO;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String crear() {
+		try {
+			categoriaDAO.create(categoria);
+			FacesMessage msg = new FacesMessage("Categoria creada");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} catch (Exception e) {
+			FacesMessage msg = new FacesMessage("Error", "Categoria no creada");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+
+		return "listarCategoria";
+	}
+
+}
