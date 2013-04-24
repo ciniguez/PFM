0ec2e3c831bfe23761ce6adea86e60c600dd5f77
macintosh <carlos.iniguez79@gmail.com>
2013-04-24 11:39:06 +0200

    Agregacion de la propiedad isAdmin en el bean pfm.beans.menu para ser utilizado en el desplegamiento de la barra de menus. Eliminaciòn de la plantilla empleado.xhtml debido a que la barra de menu se hizo dinámica y se precinde de una plantilla aparte.

--- src/pfm/beans/menu/Menu.java
+++ src/pfm/beans/menu/Menu.java
@@ -2,10 +2,23 @@ package pfm.beans.menu;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import pfm.entidades.Usuario;
 
 @ManagedBean(name = "menu")
 @RequestScoped
 public class Menu {
+	
+	public boolean getIsAdmin(){
+		Usuario usuarioSesion = (Usuario)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("UsuarioBean");
+		//TODO: Poner en enumeracion los perfiles
+		if(usuarioSesion.getRol().getId()==3)
+			return true;
+		else
+			return false;
+			
+	}
 	
 	public String listarEmpresa(){return "empresa";}
 	public String listarAgencia(){return "agencia";}
